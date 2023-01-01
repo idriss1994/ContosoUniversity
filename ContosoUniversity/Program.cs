a3b65ca152e5eed8b369e5c254c643f5da69dab2
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2023-01-01 20:26:35 +0100

    Enableing Developer page exception and migration end point

--- ContosoUniversity/Program.cs
+++ ContosoUniversity/Program.cs
@@ -8,6 +8,9 @@ builder.Services.AddRazorPages();
 builder.Services.AddDbContext<SchoolContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("SchoolContext") ?? throw new InvalidOperationException("Connection string 'SchoolContext' not found.")));
 
+// This should only be enabled in development environment
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,6 +19,11 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+    app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
 }
 
 app.UseHttpsRedirection();
