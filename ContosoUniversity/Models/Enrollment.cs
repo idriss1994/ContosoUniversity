53feae2b079d2bcafc066ed797277d98e0b82f5e
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2023-01-01 17:55:38 +0100

    Adding Enrolment model

---
+++ ContosoUniversity/Models/Enrollment.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
+{
+    public enum Grade { A, B, C, D }
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int StudentID { get; set; }
+        public int CourseID { get; set; }
+
+        [DisplayFormat(NullDisplayText = "No grade")]
+        public Grade? Grade { get; set; }
+
+        public Course Course { get; set; }
+        public Student Student { get; set; }
+    }
+}
