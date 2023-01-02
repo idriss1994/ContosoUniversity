1b78a4b19b34f4a69def9c0e77c770ce9bf6df57
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2023-01-02 22:05:48 +0100

    Adding Student view model

---
+++ ContosoUniversity/ViewModels/StudentViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.ViewModels
+{
+    public class StudentViewModel
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
