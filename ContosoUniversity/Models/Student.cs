64a16b9da1fa5e7992c8ec9c641cf0c6bad8d73d
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2023-01-02 20:42:22 +0100

    specifieing the data type of EnrollmentDate property as Date

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -1,10 +1,14 @@
-﻿namespace ContosoUniversity.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
 {
     public class Student
     {
         public int ID { get; set; }
         public string LastName { get; set; }
         public string FirstName { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime EnrollmentDate { get; set; }
 
         public ICollection<Enrollment> Enrollments { get; set; }
