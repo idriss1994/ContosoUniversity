1aa87b18d584bce47c904da232b00f46d4a7284b
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2023-01-09 17:48:01 +0100

    Adding Instructors page

---
+++ ContosoUniversity/Pages/Instructors/Index.cshtml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Data;
+using ContosoUniversity.Models;
+using ContosoUniversity.ViewModels;
+
+namespace ContosoUniversity.Pages.Instructors
+{
+    public class IndexModel : PageModel
+    {
+        private readonly SchoolContext _context;
+
+        public IndexModel(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public InstructorIndexDataViewModel InstructorData { get; set; }
+        public int InstructorID { get; set; }
+        public int CourseID { get; set; }
+
+        public async Task OnGetAsync(int? id, int? courseID)
+        {
+            InstructorData = new InstructorIndexDataViewModel();
+            InstructorData.Instructors = await _context.Instructors
+                .Include(i => i.OfficeAssignment)
+                .Include(i => i.Courses)
+                .ThenInclude(c => c.Department)
+                .OrderBy(i => i.LastName)
+                .ToListAsync();
+
+            // get Courses of specified Instructor:
+            if (id is not null)
+            {
+                InstructorID = id.Value;
+                Instructor instructor = InstructorData.Instructors
+                    .Where(i => i.ID == id.Value).Single();
+                InstructorData.Courses = instructor.Courses;
+            }
+
+            // get Enrollments include students of specified course
+            if (courseID is not null)
+            {
+                CourseID = courseID.Value;
+                IEnumerable<Enrollment> enrollments = await _context.Enrollments
+                    .Where(e => e.CourseID == CourseID)
+                    .Include(e => e.Student)
+                    .ToListAsync();
+                InstructorData.Enrollments = enrollments;
+            }
+        }
+    }
+}
