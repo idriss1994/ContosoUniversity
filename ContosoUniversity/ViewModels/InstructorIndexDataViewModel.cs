83775ef2d48ec40084530fd523fdb3972ed02355
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2023-01-09 13:12:21 +0100

    Creating InstructorIndexDataViewModel

---
+++ ContosoUniversity/ViewModels/InstructorIndexDataViewModel.cs
@@ -0,0 +1,11 @@
+﻿using ContosoUniversity.Models;
+
+namespace ContosoUniversity.ViewModels
+{
+    public class InstructorIndexDataViewModel
+    {
+        public IEnumerable<Instructor> Instructors { get; set; }
+        public IEnumerable<Course> Courses { get; set; }
+        public IEnumerable<Enrollment> Enrollments { get; set; }
+    }
+}
