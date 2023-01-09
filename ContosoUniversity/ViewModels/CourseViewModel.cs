e02791d54d527742d414af29335d2c337a0eded3
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2023-01-09 13:01:14 +0100

    Adding Course view model

---
+++ ContosoUniversity/ViewModels/CourseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.ViewModels
+{
+    public class CourseViewModel
+    {
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        [Display(Name = "Department")]
+        public string DepartmentName { get; set; }
+    }
+
+}
