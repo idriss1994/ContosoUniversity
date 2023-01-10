b4349b3bd3b1668e984e18c6063358b724358ea3
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2023-01-10 12:32:56 +0100

    Adding InstructorCourses PageModel base class to populate assigned courses data

---
+++ ContosoUniversity/Pages/Instructors/InstructorCoursesPageModel.cs
@@ -0,0 +1,29 @@
+﻿using ContosoUniversity.Data;
+using ContosoUniversity.Models;
+using ContosoUniversity.ViewModels;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContosoUniversity.Pages.Instructors
+{
+    public class InstructorCoursesPageModel : PageModel
+    {
+        public List<AssignedCourseDataViewModel> AssignedCourseDataList;
+
+        public void PopulateAssignedCourseData(SchoolContext context, Instructor instructor)
+        {
+            var allCourses = context.Courses;
+            var instructorCourses = new HashSet<int>(
+                instructor.Courses.Select(c => c.CourseID));
+            AssignedCourseDataList = new List<AssignedCourseDataViewModel>();
+            foreach (var course in allCourses)
+            {
+                AssignedCourseDataList.Add(new AssignedCourseDataViewModel
+                {
+                    CourseID = course.CourseID,
+                    Title = course.Title,
+                    Assigned = instructorCourses.Contains(course.CourseID)
+                });
+            }
+        }
+    }
+}
