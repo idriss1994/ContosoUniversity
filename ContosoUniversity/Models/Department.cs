9ac0acd95bb6bc748eb206a88b67b2bb6807483b
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2023-01-11 15:33:38 +0100

    Adding ConcurrencyToken property to Department model for handling Concurrency conflicts

--- ContosoUniversity/Models/Department.cs
+++ ContosoUniversity/Models/Department.cs
@@ -22,6 +22,9 @@ namespace ContosoUniversity.Models
 
         public int? InstructorID { get; set; }
 
+        [Timestamp]
+        public byte[] ConcurrencyToken { get; set; }
+
         public Instructor Administrator { get; set; }
         public ICollection<Course> Courses { get; set; }
     }
