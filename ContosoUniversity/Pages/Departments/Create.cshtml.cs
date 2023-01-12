2bfa909b3e997695f7582d9d01612237663df9d1
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2023-01-12 19:27:10 +0100

    Adding Department Create page

---
+++ ContosoUniversity/Pages/Departments/Create.cshtml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ContosoUniversity.Data;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.Pages.Departments
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ContosoUniversity.Data.SchoolContext _context;
+
+        public CreateModel(ContosoUniversity.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            // Fill the instructors select list with strongly list.
+            InstructorsSL = new SelectList(_context.Instructors, "ID", "FullName");
+
+            return Page();
+        }
+
+        [BindProperty]
+        public Department Department { get; set; }
+        public SelectList InstructorsSL { get; set; }
+
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                // Fill the instructors select list with strongly list.
+                InstructorsSL = new SelectList(_context.Instructors, "ID", "FullName", Department.InstructorID);
+
+                return Page();
+            }
+
+            _context.Departments.Add(Department);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
